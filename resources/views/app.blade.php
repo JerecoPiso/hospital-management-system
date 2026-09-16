c3c12b88b47aa52dc5fe28f16310a27ba0433a87
JerecoPiso <jerecojamespiso@gmail.com>
2026-09-16 10:19:34 +0800

    dynamic page name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hospital Management System</title>
+    <title>{{ env('VITE_FACILITY_NAME', 'Hospital Management System') }}</title>
     <link rel="icon" type="image/svg+xml" href="/hospital-management-system/favicon.svg">
     <link rel="alternate icon" href="/hospital-management-system/favicon.ico">
     @vite('resources/css/app.css')
