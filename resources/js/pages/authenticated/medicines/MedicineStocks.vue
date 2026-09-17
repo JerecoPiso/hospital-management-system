e5c2ab0227e76617d48bf93fe9aa6df212b6e190
JerecoPiso <jerecojamespiso@gmail.com>
2026-09-17 13:43:44 +0800

    fix the expiration issue in stocks and medicine

--- resources/js/pages/authenticated/medicines/MedicineStocks.vue
+++ resources/js/pages/authenticated/medicines/MedicineStocks.vue
@@ -141,7 +141,7 @@
       <Column field="reorder_level" header="Reorder Lvl" class="w-28" />
       <Column header="Expiration" class="w-32">
         <template #body="{ data }"
-          ><span class="text-slate-600 text-sm">{{ data.expiration_date.split("T")[0] || "—" }}</span></template
+          ><span class="text-slate-600 text-sm">{{ data.expiration_date?.split("T")[0] || "—" }}</span></template
         >
       </Column>
       <Column header="Actions" class="w-24">
