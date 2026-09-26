c6c969583b20341085001fd180d527b292906e70
JerecoPiso <jerecojamespiso@gmail.com>
2026-09-26 17:47:52 +0800

    add rbac on invoices

--- resources/js/pages/authenticated/patientchart/PatientInformation.vue
+++ resources/js/pages/authenticated/patientchart/PatientInformation.vue
@@ -1274,7 +1274,9 @@ onMounted(async () => {
       toast.error(err.response?.data?.message || "Failed to load ICD codes");
     }
   }
-  await loadInvoices()
+  if (can("invoices", "view")) {
+    await loadInvoices();
+  }
 });
 
 // SOAP Notes
